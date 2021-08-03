9932a89d48d851d38dc0742e68b4931f6d79fe76
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-03 14:39:25 -0500

    Funcionalidad para cerrar el modal

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const button1 = $("#button1");
 const button2 = $("#button2");
 const button3 = $("#button3");
 const modal = $(".modal");
+const closeModal = $(".close-modal");
 
 button1.onclick = () => {
   modal.classList.remove("hidden");
@@ -18,4 +19,9 @@ button2.onclick = () => {
 button3.onclick = () => {
   modal.classList.remove("hidden");
   modal.classList.add("visible");
+};
+
+closeModal.onclick = () => {
+  modal.classList.remove("visible");
+  modal.classList.add("hidden");
 };
